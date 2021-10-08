6d05e0ee331399dba9070b6b85ad6c295c1a2152
mathe <matheusim27@hotmail.com>
2021-10-07 21:58:55 -0300

    Começando page Meus links

--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+
 import StatusBarPage from '../../components/StatusBarPage';
+import ListItem from '../../components/ListItem';
+
+import { Container, Title, ListLinks } from './styles';
+
+import Menu from '../../components/Menu';
 
 export default function MyLinks() {
   return (
-    <View>
+    <Container>
       <StatusBarPage barStyle="light-content" backgroundColor="#1ddbb9" />
-      <Text>MyLinks</Text>
-    </View>
+
+      <Menu />
+      <Title>Meus Links</Title>
+
+      <ListLinks
+        data={[
+          { id: 1, link: 'test.com' },
+          { id: 2, link: 'testando.com' },
+        ]}
+        keyExtractor={({ item }) => String(item.id)}
+        renderItem={({ item }) => <ListItem data={item} />}
+        contentContainerStyle={{ paddinBottom: 20 }}
+        showVerticalScrollIndicator={false}
+      />
+    </Container>
   );
 }
